4894730e7743899ed64fa28c680fdb135bbf0c26
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2025-06-26 07:15:56 +0530

    .

--- basics/04_.js
+++ basics/04_.js
@@ -1,7 +1,4 @@
 // let score = undefined
-
-
-
 // console.log(score)
 // console.log(typeof score)
 
@@ -9,23 +6,97 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN
-// true =>1; false => 0;
+// // "33" => 33
+// // "33abc" => NaN
+// // true =>1; false => 0;
 
 // let isLoggedIn = 1
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-// let isLoggedIn = " mamta "
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedInt = " mamta "
+// let booleanIsLoggedInt = Boolean(isLoggedInt)
 // console.log(booleanIsLoggedIn);
 
-// 1=> true; 0 => false;
-// "" => false;
-// "mamta" => true 
+//  // 1=> true; 0 => false;
+// // "" => false;
+// // "mamta" => true 
 
-let someNumber = 32
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 32
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// // ************************Operations*********************//
+
+// let value =3
+// let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+3);
+// console.log(5-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(6%2);
+// console.log(2**3);
+
+// let str1 ="hello"
+// let str2 ="world"
+// let str3 =str1 + " " + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 1 + "2");
+
+// console.log(2 + 4 * (50%5));
+
+// console.log(+true);
+// console.log(+ "");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+// let gameCounters = 100
+// gameCounters++;
+// console.log(gameCounters);
+
+// let gameCounter1= 100
+// gameCounter1--;
+// console.log(gameCounter1);
+
+// let gameCounter2 = 100
+// --gameCounter2;
+// console.log(gameCounter2);
+
+// let x=3;
+// const y = x++;
+
+// console.log(y, x);
+
+console.log(2>1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log(null<0);
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log("2">1);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+
+// ===
+console.log("2" === 1);
+
+// comparisions are aboidable in many cases (clean code are most priorities)
