2b49a303451ebea4a433d60022001a53507f6288
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2025-06-26 13:01:29 +0530

    comaprison

---
+++ basics/05_Data_types.js
@@ -0,0 +1,55 @@
+console.log("Mamta Gupta")
+
+//DATA TYPES
+// JavaScript is a Dynamic Type language:
+// because:->  The type of a variable is determined at runtime, and can change during execution.
+
+
+// Primitive Data Types
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 8877543236756780n
+
+ 
+
+//  Reference (Non- Primitives) Data Types
+//Array, Objects, Functions 
+
+let myObj = {
+    Name: "mamta",
+    Age: 22,
+}
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+console.log(typeof outsideTemp);
+
+console.log(typeof scoreValue);
+
+console.log(typeof myFunction);
+
+
+//---------------------------------!
+//!           !                    !
+//!           !                    !
+//!           !                    !
+//---------------------------------!
+
